2995b9c20570b57cad75d4ad58babbffaf291439
ali_hassam <alykhanhssm@gmail.com>
2018-11-28 12:40:37 -0700

    fixed

--- index.php
+++ index.php
@@ -20,26 +20,4 @@ if ($conn->query($sql) === TRUE) {
 }
 
 $conn->close();
-
-<?php
-$servername = "localhost";
-$username = "admin";
-$password = "password";
-
-// Create connection
-$conn = new mysqli($servername, $username, $password);
-// Check connection
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-} 
-
-// Create database
-$sql = "CREATE DATABASE TutorScheduleDB";
-if ($conn->query($sql) === TRUE) {
-    echo "Tutor Database created successfully";
-} else {
-    echo "Error creating database: " . $conn->error;
-}
-
-$conn->close();
 ?>
