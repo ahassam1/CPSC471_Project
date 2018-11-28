4542e47ad4092cc59934324d405c167777f4ddfb
ali_hassam <alykhanhssm@gmail.com>
2018-11-28 12:39:45 -0700

    Update index.php

--- index.php
+++ index.php
@@ -13,29 +13,7 @@ if ($conn->connect_error) {
 // Create database
 $sql = "CREATE DATABASE TutorScheduleDB";
 if ($conn->query($sql) === TRUE) {
-    echo "Database created successfully";
-} else {
-    echo "Error creating database: " . $conn->error;
-}
-
-$conn->close();
-
-<?php
-$servername = "localhost";
-$username = "username";
-$password = "password";
-
-// Create connection
-$conn = new mysqli($servername, $username, $password);
-// Check connection
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-} 
-
-// Create database
-$sql = "CREATE DATABASE myDB";
-if ($conn->query($sql) === TRUE) {
-    echo "Database created successfully";
+    echo "Tutor Database created successfully";
 } else {
     echo "Error creating database: " . $conn->error;
 }
