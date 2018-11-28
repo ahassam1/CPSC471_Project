b12ed3711a916e734ca6b568d266caf6c516a261
ali_hassam <alykhanhssm@gmail.com>
2018-11-28 13:33:34 -0700

    comments

--- index.php
+++ index.php
@@ -25,14 +25,14 @@ $conn = new mysqli($servername, $username, $password, "TutorScheduleDB");
 
 //how to use foreign keys https://stackoverflow.com/questions/22211452/getting-syntax-error-when-declaring-foreign-keys-in-mysql-using-innodb
 
-$sql = "CREATE TABLE CLIENT (
+$sql = "CREATE TABLE IF NOT EXISTS CLIENT (
 		SIN INT(9) PRIMARY KEY,
-		Name VARCHAR(30)
+		Name VARCHAR(30) NOT NULL
 		)";
 		
 	if ($conn->query($sql) === TRUE) 
 	{
-		echo "Table MyGuests created successfully";
+		echo "Table Client created successfully";
 	} 
 	else 
 	{
