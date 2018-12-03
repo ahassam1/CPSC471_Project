81f14a47d6e7a4ec626c19d722c7b60e2f134758
ali_hassam <alykhanhssm@gmail.com>
2018-12-03 14:34:01 -0700

    Updated userverify

--- userverify.php
+++ userverify.php
@@ -4,6 +4,22 @@
 		$Username = $_POST['username'];
 		$Password = $_POST['pass'];
 		
+		session_start(); //Never forget this line when using $_SESSION
+		
+		$_SESSION['username'] = $Username;
+		$_SESSION['password'] = $Password;
+		
+		$sql = "SELECT L.Username, L.Password, L.User_SIN
+			   From Login as L
+			   Where $Username = L.Username and $Password = L.Password";
+			   
+		$conn->query($sql);
+
+		
+
+		
+
+		
 		
 		//header('location:index.php');
 	?>
