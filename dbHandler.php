103fa119ae9b850c3d6298a92b5eb6989c77ec5a
U-WAFA\Wafa <anam_wafa@yahoo.ca>
2018-12-03 13:00:50 -0700

    reset to master

---
+++ dbHandler.php
@@ -0,0 +1,10 @@
+<?php
+
+$servername = "localhost";
+$username = "admin";
+$password = "password";
+$dbName = "TutorScheduleDB";
+
+$conn = mysqli_connect($servername, $username, $password, $dbName);
+
+?>
